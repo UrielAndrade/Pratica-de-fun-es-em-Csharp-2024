46c9bb0ad8d8a0fe77715ac46f693bc8f52a65b4
Leiru <155118208+UrielAndrade@users.noreply.github.com>
2024-11-19 08:34:58 -0400

    Create Atividade para praticar-1.cs

---
+++ Atividade para praticar-1.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividades_fixacao_FUNCTIONS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //atividade-1//
+            /*
+            Console.WriteLine($"Digite o número: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"O numero foi {numero}, e ficou {RetornarNumNegativo(numero)}");
+            Console.ReadKey();
+            */
+
+            //atividade-2//
+            /*
+            calcPotencia();
+            */
+            //atividade-3//
+            Console.Write($"Digite o numero: ");
+            int numero = Convert.ToInt32( Console.ReadLine() );
+            Console.WriteLine($"O fatorial do numero {numero} é {fatorial(numero)}");
+            Console.ReadKey();
+        }
+
+        //funçoes atividade-1//
+        static int RetornarNumNegativo(int num)
+        {
+            if (num <= 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        //funçoes atividade-2//
+        static void calcPotencia()
+        {
+            Console.Write("Digite o BASE: ");
+            int basi = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite o EXPOENTE: ");
+            int expoente = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"A potencia da operação de BASE: {basi} e de EXPOENTE: {expoente} é {Math.Pow(basi, expoente)}");
+        }
+
+        static int fatorial(int num)
+        {
+            if (num > 0) 
+            {
+                int fac = 1;
+                for (int i = num; i !=0; i--)
+                {
+                    fac = num * fac;
+                    Console.WriteLine($"{num} X {num} = {fac}");
+                    num = num--;
+                }
+                return fac;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+
+    }
+}
